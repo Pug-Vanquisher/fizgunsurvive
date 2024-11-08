6a88fef61a398fe8fe91ae746502625dc198f63c
HolyDeemon <PPVDmit@yandex.ru>
2024-11-09 00:47:48 +0300

    ChaosUpdate

---
+++ SkibidiPushka/Assets/Scripts/Timo/CorrupterPlacer.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrupterPlacer : MonoBehaviour
+{
+    public GameObject CorrupterPrefab;
+    public GameObject[] ObjectsPrefabs;
+    public int enemySpawnrate;
+    public int obstalceSpawnrate;
+    public float respawnMaxDistance;
+    public float respawnMinDistance;
+    void Start()
+    {
+        EventManager.Instance.Subscribe("CorruptTiles", StartTileRemake);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemySpawnrate + obstalceSpawnrate != transform.childCount)
+        {
+            for (int i = 0; i < Mathf.Abs(enemySpawnrate + obstalceSpawnrate - transform.childCount); i++)
+            {
+                if (transform.childCount > enemySpawnrate + obstalceSpawnrate)
+                {
+                    Destroy(transform.GetChild(transform.childCount - i - 1).gameObject);
+                }
+                else if (transform.childCount < enemySpawnrate + obstalceSpawnrate)
+                {
+                    var a = Instantiate(CorrupterPrefab, transform);
+                    a.GetComponent<EnvCorrupter>().localPos = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized * Random.Range(respawnMinDistance, respawnMaxDistance);
+                }
+            }
+        }
+    }
+    void StartTileRemake()
+    {
+        Invoke("TileRemake", 0.1f);
+    }
+    void TileRemake()
+    {
+        int enemies = enemySpawnrate;
+        int obstacles = obstalceSpawnrate;
+
+        while(enemies + obstacles > 0)
+        {
+            int rnd = Random.Range(0, enemies + obstacles);
+            if(rnd < enemies)
+            {
+                Summon(0, transform.GetChild(enemies + obstacles-1).transform.position);
+                enemies -= 1;
+            }
+            else if(rnd >= enemies)
+            {
+                Summon(Random.Range(1, ObjectsPrefabs.Length), transform.GetChild(enemies + obstacles-1).transform.position);
+                obstacles -= 1;
+            }
+        }
+
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<EnvCorrupter>().localPos = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized * Random.Range(respawnMinDistance, respawnMaxDistance);
+        }
+    }
+
+
+
+    void Summon(int _id, Vector2 _position)
+    {
+        Instantiate(ObjectsPrefabs[_id], _position, Quaternion.identity);
+    }
+
+}
---
+++ SkibidiPushka/Assets/Scripts/Timo/EnvCorrupter.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvCorrupter : MonoBehaviour
+{
+    private Dictionary<Collider2D, GameObject> collidersIn = new Dictionary<Collider2D, GameObject>();
+    public Vector2 localPos;
+    void Start()
+    {
+        EventManager.Instance.Subscribe("CorruptTiles", KillTile);
+    }
+    private void Update()
+    {
+        transform.position = tiled(localPos + new Vector2(transform.parent.position.x, transform.parent.position.y));
+    }
+    void KillTile()
+    {
+        List<GameObject> gmbj = new List<GameObject>();
+        foreach (KeyValuePair<Collider2D, GameObject> pair in collidersIn)
+        {
+            gmbj.Add(pair.Value);
+        }
+        foreach (GameObject gm in gmbj)
+        {
+            Destroy(gm);
+        }
+        collidersIn.Clear();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!collidersIn.ContainsKey(collision))
+        {
+            collidersIn.Add(collision, collision.gameObject);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collidersIn.Remove(collision);
+    }
+    Vector2 tiled(Vector2 pos)
+    {
+        return new Vector2((int)pos.x, (int)pos.y);
+    }
+}
