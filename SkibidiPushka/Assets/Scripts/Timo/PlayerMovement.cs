850527cc81f5258e898725c1369937457c5be633
Miri-Solari <horniret@gmail.com>
2024-11-11 01:55:48 +0300

    Animation for Player

--- SkibidiPushka/Assets/Scripts/Timo/PlayerMovement.cs
+++ SkibidiPushka/Assets/Scripts/Timo/PlayerMovement.cs
@@ -1,25 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] Rigidbody2D playerBody;
     [SerializeField] float speed;
+    [SerializeField] Animator animator;
     protected Vector3 direct;
-    void Update()
+    private bool _isNoFlipped = true;
+
+    private void Start()
+    {
+        EventManager.Instance.Subscribe("StartAttack", Attack);
+        EventManager.Instance.Subscribe("StopAttack", StopAttack);
+    }
+    void FixedUpdate()
     {
         direct.x = Input.GetAxis("Horizontal");
         direct.y = Input.GetAxis("Vertical");
-        MoveTest(direct.normalized);
-        
+        Move();
+        Flip();
+
     }
 
-    private void MoveTest(Vector3 direct)
+    private void Move()
     {
-        playerBody.velocity = direct * speed;
+        playerBody.velocity = direct.normalized * speed * Time.fixedDeltaTime * 50;
+        animator.SetFloat("Velocity", playerBody.velocity.magnitude);
     }
 
-   
+    void Flip()
+    {
+        Debug.Log(playerBody.velocity.ToString() + _isNoFlipped);
+        if (playerBody.velocity.x < 0 && _isNoFlipped)
+        {
+            playerBody.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            _isNoFlipped = false;
+        }
+        else if (playerBody.velocity.x > 0 && !_isNoFlipped)
+        {
+            playerBody.transform.localRotation = Quaternion.Euler(0, 180, 0);
+            _isNoFlipped = true;
+        }
+    }
+
+    void Attack()
+    {
+        animator.SetFloat("Attack", 1);
+    }
+
+    void StopAttack()
+    {
+        animator.SetFloat("Attack", -1);
+    }
+
+    private void OnDestroy()
+    {
+        EventManager.Instance.Unsubscribe("StartAttack", Attack);
+        EventManager.Instance.Unsubscribe("StopAttack", StopAttack);
+    }
+
 }
 
