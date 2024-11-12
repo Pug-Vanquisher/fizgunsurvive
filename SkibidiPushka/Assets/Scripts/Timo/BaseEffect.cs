3fbb60111febbf2e3e28a877ae93bf9c8e27126a
HolyDeemon <PPVDmit@yandex.ru>
2024-11-12 12:14:43 +0300

    UpgradeUpdate

--- SkibidiPushka/Assets/Scripts/Timo/BaseEffect.cs
+++ SkibidiPushka/Assets/Scripts/Timo/BaseEffect.cs
@@ -1,34 +1,49 @@
 using UnityEngine;
+using System.Collections.Generic;
+[System.Serializable]
 
-public class BaseEffect : MonoBehaviour // потом засунуть в наследование каждого уникального эффекта
+public class BaseEffect// потом засунуть в наследование каждого уникального эффекта
 {
-    [SerializeField] internal string Name;
+    public string EventName;
+    public string Description;
+    public Color Color;
+    public string Event;
 
-    private void Awake()
+    public BaseEffect(string _name, string _description, Color _color, string _EventName)
     {
-        EventManager.Instance.Subscribe(Name, StartEffect);
-        EventManager.Instance.Subscribe(Name + "Stop", StopEffect);
+        EventName = _name;
+        Description = _description;
+        Color = _color;
+        Event = _EventName;
     }
-
-    protected virtual void StartEffect()
-    {
-
-    }
-
-    protected virtual void StopEffect()
-    {
-
-    }
-
-    protected virtual void Effect()
-    {
-
-    }
-    private void OnDestroy()
-    {
-        EventManager.Instance.Unsubscribe(Name, StartEffect);
-        EventManager.Instance.Unsubscribe(Name + "Stop", StopEffect);
-    }
-
-
 }
+
+public class DatabaseEffects
+{
+    public static List<BaseEffect> effects = new List<BaseEffect>()
+    {
+        new BaseEffect( "Повышение активности",
+            "Появление большего количества врагов",
+            Color.red, "HighActivity"),
+        new BaseEffect("Повышение нагрузки",
+            "Появление большего количества обьектов",
+            new Color(0.19f, 1f, 1f), "HighLoad"),
+        new BaseEffect("Повышение опасности",
+            "Появление большего количества пуль и ракеты.",
+            Color.yellow, "HighDanger"),
+        new BaseEffect("Повышение безопасности",
+            "Появление большего количества аптечек первой помощи",
+            new Color(0.5f, 0.8f, 0,45f), "HighSequrity"),
+        new BaseEffect("Повышение нестабильности",
+            "Реальность дрожит, разрываясь большим количеством разломов",
+            Color.magenta, "HighVolatility"),
+        new BaseEffect("Повышение подвижности",
+            "Повышение скорости пользователя",
+            Color.cyan, "HighMobility"),
+        new BaseEffect("Повышение темпа",
+            "Повышение скорости стрельбы оружия",
+            new Color(0.6f, 1f, 0.5f), "HighTemp")
+    };
+    public static Color LastUpgradeColor = Color.white;
+}
+
