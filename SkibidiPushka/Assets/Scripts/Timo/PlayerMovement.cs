c1c4e6d709bee00bc91d8aaedb571d90baf29b10
Pug-Vanquisher <mikhail.markov.8484@mail.ru>
2024-11-12 17:16:49 +0300

    GhostMode

--- SkibidiPushka/Assets/Scripts/Timo/PlayerMovement.cs
+++ SkibidiPushka/Assets/Scripts/Timo/PlayerMovement.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using System.Collections;
 
-
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] Rigidbody2D playerBody;
@@ -14,31 +13,43 @@ public class PlayerMovement : MonoBehaviour
     protected Vector3 direct;
     private bool _isNoFlipped = true;
     private WaitForFixedUpdate _waiter = new WaitForFixedUpdate();
+    private int initialLayer;
 
     private void Start()
     {
         EventManager.Instance.Subscribe("StartAttack", Attack);
         EventManager.Instance.Subscribe("StopAttack", StopAttack);
+        EventManager.Instance.Subscribe("HighMobility", SpeedUpscale);
+        EventManager.Instance.Subscribe("Ghost", GhostMode);
+
+        initialLayer = gameObject.layer;
     }
+
     void Update()
     {
         direct.x = Input.GetAxis("Horizontal");
         direct.y = Input.GetAxis("Vertical");
         if (Input.GetKeyDown(dashKey))
         {
-            Debug.Log("sdfjdjklhsfgsjhfg;lsdg");
+            Debug.Log("Dash activated");
             _currDashTime = 0;
             StartCoroutine(Dash(direct.normalized));
         }
-        else { Move(); }
-        
+        else
+        {
+            Move();
+        }
+    }
 
+    void SpeedUpscale()
+    {
+        speed += 2;
     }
 
     private void Move()
     {
         playerBody.velocity = direct.normalized * speed * Time.fixedDeltaTime * 50;
-        animator.SetFloat("Speed", playerBody.velocity.magnitude/speed);
+        animator.SetFloat("Speed", playerBody.velocity.magnitude / speed);
         animator.SetFloat("X", playerBody.velocity.normalized.x);
     }
 
@@ -56,6 +67,8 @@ public class PlayerMovement : MonoBehaviour
     {
         EventManager.Instance.Unsubscribe("StartAttack", Attack);
         EventManager.Instance.Unsubscribe("StopAttack", StopAttack);
+        EventManager.Instance.Unsubscribe("HighMobility", SpeedUpscale);
+        EventManager.Instance.Unsubscribe("Ghost", GhostMode);
     }
 
     private IEnumerator Dash(Vector3 direct)
@@ -69,5 +82,18 @@ public class PlayerMovement : MonoBehaviour
         yield return null;
     }
 
-}
+    private void GhostMode()
+    {
+        StartCoroutine(TemporaryGhostMode());
+    }
 
+    private IEnumerator TemporaryGhostMode()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+
+        yield return new WaitForSeconds(5);
+
+        gameObject.layer = initialLayer;
+
+    }
+}
